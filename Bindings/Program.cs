2a5b7e5e0ea8b6a229c896028609e16714de127f
ChrisDill <chris.rj.dill@gmail.com>
2018-10-05 18:00:12 +0100

    Bindings project can now test directly
    - Changed back to exe project so bindings can be tested easily. Otherwise build as a class library.
    - raylib.dll moved yet again. It is now in with bindings and copied to output.

---
+++ Bindings/Program.cs
@@ -0,0 +1,44 @@
+﻿using Raylib;
+using static Raylib.Raylib;
+
+// example to quickly test bindings
+// otherwise build as a class library to use in other projects
+class Program
+{ 
+    public static void Main()
+    {
+        // Initialization
+        //--------------------------------------------------------------------------------------
+        int screenWidth = 800;        int screenHeight = 450;
+
+        InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+        SetTargetFPS(60);
+        //--------------------------------------------------------------------------------------
+
+        // Main game loop
+        while (!WindowShouldClose())    // Detect window close button or ESC key
+        {
+            // Update
+            //----------------------------------------------------------------------------------
+            // TODO: Update your variables here
+            //----------------------------------------------------------------------------------
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, MAROON);
+
+            EndDrawing();
+            //----------------------------------------------------------------------------------
+        }
+
+        // De-Initialization
+        //--------------------------------------------------------------------------------------   
+        CloseWindow();        // Close window and OpenGL context
+                              //--------------------------------------------------------------------------------------
+    }
+}
