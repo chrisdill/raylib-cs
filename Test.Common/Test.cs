3af8accf9b8db0040282a0730696b08b3ed799e1
msmshazan <msmshazan1@gmail.com>
2018-10-21 23:04:32 +0530

    added new runtimes

---
+++ Test.Common/Test.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Raylib;
+using static Raylib.Raylib;
+
+namespace Test.Common
+{
+    public static class Test
+    {
+        public static void Run()
+        {
+            // Initialization
+            //--------------------------------------------------------------------------------------
+            int screenWidth = 800;
+            int screenHeight = 450;
+
+            // SetConfigFlags((int)Flag.WINDOW_UNDECORATED);
+            InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+            var model = LoadModel("bridge.obj");
+            model.mesh.Vertices[0] = 5f;
+
+            SetTargetFPS(60);
+            //--------------------------------------------------------------------------------------      
+
+            // Main game loop
+            while (!WindowShouldClose())    // Detect window close button or ESC key
+            {
+                // Update
+                //----------------------------------------------------------------------------------
+                // TODO: Update your variables here
+                //----------------------------------------------------------------------------------
+
+                // Draw
+                //----------------------------------------------------------------------------------
+                BeginDrawing();
+
+                ClearBackground(RAYWHITE);
+
+                DrawText("Congrats! You created your first window!", 190, 200, 20, MAROON);
+
+                EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+
+            // De-Initialization
+            //--------------------------------------------------------------------------------------   
+            CloseWindow();        // Close window and OpenGL context
+                                  //--------------------------------------------------------------------------------------
+        }
+
+    }
+}
---
+++ Test.NetFX/Program.cs
@@ -0,0 +1,12 @@
+﻿using Test.Common;
+
+namespace Test.NetFX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Common.Test.Run();
+        }
+    }
+}
