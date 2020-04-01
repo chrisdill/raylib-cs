943981e8cd25e220c4726cb9031962deeb550518
ChrisDill <chris.rj.dill:gmail.com>
2020-04-01 17:24:54 +0100

    Updated Tests project. - Renamed from Tests.MultiTarget to Tests. - References Raylib-cs.csproj instead of adding bindings directly.

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,5 +1,5 @@
-﻿using Raylib;
-using static Raylib.Raylib;
+﻿using Raylib_cs;
+using static Raylib_cs.Raylib;
 
 namespace Tests
 {
@@ -11,12 +11,12 @@ namespace Tests
             //--------------------------------------------------------------------------------------
             const int screenWidth = 800;
             const int screenHeight = 450;
-        
+
             InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-        
+
             SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
             //--------------------------------------------------------------------------------------
-        
+
             // Main game loop
             while (!WindowShouldClose())    // Detect window close button or ESC key
             {
@@ -24,24 +24,24 @@ namespace Tests
                 //----------------------------------------------------------------------------------
                 // TODO: Update your variables here
                 //----------------------------------------------------------------------------------
-        
+
                 // Draw
                 //----------------------------------------------------------------------------------
                 BeginDrawing();
-        
+
                     ClearBackground(Color.RAYWHITE);
-        
+
                     DrawText("Congrats! You created your first window!", 190, 200, 20, Color.LIGHTGRAY);
-        
+
                 EndDrawing();
                 //----------------------------------------------------------------------------------
             }
-        
+
             // De-Initialization
             //--------------------------------------------------------------------------------------
             CloseWindow();        // Close window and OpenGL context
             //--------------------------------------------------------------------------------------
-        
+
             return;
         }
     }
