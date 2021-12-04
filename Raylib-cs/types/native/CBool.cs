fb6c60cfc2caec9a8fa283a8b81daeebfa7c5644
Ben Parsons <9parsonsb@gmail.com>
2021-12-05 01:46:54 +1100

    Split CBool from utils

---
+++ Raylib-cs/types/native/CBool.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Raylib_cs;
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct CBool
+{
+    private readonly byte value;
+
+    private CBool(bool value)
+    {
+        this.value = Convert.ToByte(value);
+    }
+
+    public static implicit operator CBool(bool value)
+    {
+        return new CBool(value);
+    }
+
+    public static implicit operator bool(CBool x)
+    {
+        return Convert.ToBoolean(x.value);
+    }
+
+    public override string ToString()
+    {
+        return Convert.ToBoolean(value).ToString();
+    }
+}
