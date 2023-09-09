3518ffa3f6c4897810b0b9d67981e4d073caa5ab
Nickolas McDonald <43690021+n77y@users.noreply.github.com>
2023-09-09 12:38:00 -0400

    remove MeshExtentions class

--- Raylib-cs/types/Mesh.cs
+++ Raylib-cs/types/Mesh.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace Raylib_cs;
@@ -64,6 +65,112 @@ public unsafe partial struct Mesh
     /// Vertex indices (in case vertex data comes indexed)
     /// </summary>
     public ushort* Indices = default;
+
+    /// <summary>
+    /// Allocate <see cref="Vertices"/>
+    /// </summary>
+    public void AllocVertices()
+    {
+        Vertices = Raylib.New<float>(3 * VertexCount);
+    }
+
+    /// <summary>
+    /// Allocate <see cref="TexCoords"/>
+    /// </summary>
+    public void AllocTexCoords()
+    {
+        TexCoords = Raylib.New<float>(2 * VertexCount);
+    }
+
+    /// <summary>
+    /// Allocate <see cref="TexCoords2"/>
+    /// </summary>
+    public void AllocTexCoords2()
+    {
+        TexCoords2 = Raylib.New<float>(2 * VertexCount);
+    }
+
+    /// <summary>
+    /// Allocate <see cref="Normals"/>
+    /// </summary>
+    public void AllocNormals()
+    {
+        Normals = Raylib.New<float>(3 * VertexCount);
+    }
+
+    /// <summary>
+    /// Allocate <see cref="Tangents"/>
+    /// </summary>
+    public void AllocTangents()
+    {
+        Tangents = Raylib.New<float>(4 * VertexCount);
+    }
+
+    /// <summary>
+    /// Allocate <see cref="Colors"/>
+    /// </summary>
+    public void AllocColors()
+    {
+        Colors = Raylib.New<byte>(4 * VertexCount);
+    }
+
+    /// <summary>
+    /// Allocate <see cref="Indices"/>
+    /// </summary>
+    public void AllocIndices()
+    {
+        Indices = Raylib.New<ushort>(3 * TriangleCount);
+    }
+
+    /// <summary>
+    /// Access <see cref="Vertices"/>
+    /// </summary>
+    public readonly Span<T> VerticesAs<T>() where T : unmanaged
+    {
+        return new(Vertices, 3 * VertexCount * sizeof(float) / sizeof(T));
+    }
+
+    /// <summary>
+    /// Access <see cref="TexCoords"/>
+    /// </summary>
+    public readonly Span<T> TexCoordsAs<T>() where T : unmanaged
+    {
+        return new(TexCoords, 2 * VertexCount * sizeof(float) / sizeof(T));
+    }
+
+    /// <summary>
+    /// Access <see cref="TexCoords2"/>
+    /// </summary>
+    public readonly Span<T> TexCoords2As<T>() where T : unmanaged
+    {
+        return new(TexCoords2, 2 * VertexCount * sizeof(float) / sizeof(T));
+    }
+
+    /// <summary>
+    /// Access <see cref="Normals"/>
+    /// </summary>
+    public readonly Span<T> NormalsAs<T>() where T : unmanaged
+    {
+        return new(Normals, 3 * VertexCount * sizeof(float) / sizeof(T));
+    }
+
+    /// <summary>Access <see cref="Tangents"/></summary>
+    public readonly Span<T> TangentsAs<T>() where T : unmanaged
+    {
+        return new(Tangents, 4 * VertexCount * sizeof(float) / sizeof(T));
+    }
+
+    /// <summary>Access <see cref="Colors"/></summary>
+    public readonly Span<T> ColorsAs<T>() where T : unmanaged
+    {
+        return new(Colors, 4 * VertexCount * sizeof(byte) / sizeof(T));
+    }
+
+    /// <summary>Access <see cref="Indices"/></summary>
+    public readonly Span<T> IndicesAs<T>() where T : unmanaged
+    {
+        return new(Indices, 3 * TriangleCount * sizeof(ushort) / sizeof(T));
+    }
 
     #endregion
 
